ca2c94f8ab91b226164217246ef715456d40ffac
mishraarjun <arjun@xoniertechnologies.com>
2024-08-06 15:19:19 +0530

    updated code

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Service;
 use App\Http\Requests\{StoreServiceRequest,UpdateServiceRequest};
-use App\Http\Services\DoctorServiceServices;
+use App\Http\Services\DoctorService;
 
 class ServiceController extends Controller
 {
     private $doctor_service_services;
 
-    public function __construct(DoctorServiceServices $doctor_service_services){
+    public function __construct(DoctorService $doctor_service_services){
         $this->doctor_service_services = $doctor_service_services;
     }
     public function index()
--- resources/views/doctor/my-patient/doctor-patients.blade.php
+++ resources/views/doctor/my-patient/doctor-patients.blade.php
@@ -12,75 +12,28 @@
         </ul>
     </div>
     <div class="appointment-tab-head">
-        <div class="appointment-tabs">
-            <p style="display:none" id="doctor-id">{{ $doctorDetails->id }}</p>
-            <p style="display:none" id="selected-filter">regular</p>
-            <ul class="nav nav-pills inner-tab " id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="regular" type="button" onclick="appointment_filter(this)">All
-                        patient<span>{{ count($patients) }}</span></button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="regular" type="button" onclick="appointment_filter(this)">Regular
-                        patient<span>{{ $regularPatients }}</span></button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link " id="new" type="button" onclick="appointment_filter(this)">New
-                        patient<span> {{ $newPatients ?? 0 }}</span></button>
-                </li>
-
-            </ul>
+        <div class="appointment-tab-head">
+            <div class="appointment-tabs">
+                <ul class="nav nav-pills inner-tab " id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="all_tab" data-bs-toggle="pill" data-bs-target="#pills-upcoming"
+                            type="button" role="tab" aria-controls="pills-upcoming" aria-selected="false"
+                            tabindex="-1">All Patients<span>{{ count($finalData['allPatients']) }}</span></button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="new_tab" data-bs-toggle="pill" data-bs-target="#pills-complete"
+                            type="button" role="tab" aria-controls="pills-complete" aria-selected="false"
+                            tabindex="-1">New Patients<span>{{ count($finalData['newPatients']) }}</span></button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="regular_tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-cancel" type="button" role="tab" aria-controls="pills-cancel"
+                            aria-selected="true">Regular
+                            Patients<span>{{ count($finalData['regularPatients']) }}</span></button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
-
     @include('doctor.my-patient.list')
-@endsection
-
-@section('javascript')
-<script>
-    $("#searchKey").keyup(function() {
-        filter();
-    });
-
-    // $('input[type=date]').change(function() {
-    //     filter();
-    // });
-
-    function appointment_filter(element) {
-        document.querySelectorAll('.nav-link').forEach(function(navLink) {
-            navLink.classList.remove('active');
-        });
-        element.classList.add('active');
-        jQuery('#selected-filter').text(jQuery(element).attr('id'));
-        filter();
-    }
-
-    function filter() {
-        let key = jQuery('#selected-filter').text();
-        let userId = jQuery('#doctor-id').text();
-        let searchKey = jQuery('#searchKey').val();
-        let dateSearch = jQuery('#dateSearch').val();
-        $.ajax({
-            url: "<?= route('doctor.filter.on.my-patient') ?>",
-            type: 'get',
-            data: {
-                key: key,
-                doctorId: userId,
-                searchKey: searchKey,
-                dateSearch: dateSearch,
-                _token: '{{ csrf_token() }}'
-            },
-            success: function(response) {
-
-                jQuery('#my-patient-list').replaceWith(response.data);
-                jQuery('#my-patient-list').hide().delay(200).fadeIn();
-            },
-            error: function(xhr, status, error) {
-                // Handle any errors
-                console.error(error);
-            }
-        });
-    }
-</script>
-
 @endsection

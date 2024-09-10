89f9f8f8bb4ed3e06844d8e8e6fe50a6ea599ceb
mishraarjun <arjun@xoniertechnologies.com>
2024-09-10 16:36:25 +0530

    payment Details api created

--- app/Http/Controllers/Api/PaymentApiController.php
+++ app/Http/Controllers/Api/PaymentApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Exception;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Services\PaypalService;
@@ -9,6 +10,7 @@ use App\Http\Services\PaymentService;
 use Illuminate\Support\Facades\Crypt;
 use App\Http\Services\BookingServices;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\GetBookingFeeAndCheckAuth;
 
 class PaymentApiController extends Controller
@@ -57,8 +59,7 @@ class PaymentApiController extends Controller
     {
         $paypalToken = $request->query('token') ?? '';
 
-        if(empty($paypalToken))
-        {
+        if (empty($paypalToken)) {
             return response()->json([
                 "status"    => false,
                 "error"     =>  'Invalid token',
@@ -69,19 +70,16 @@ class PaymentApiController extends Controller
         // Get the payment details based on provided token
         $paymentDetails = $this->paypalService->getPaymentDetails($paypalToken);
 
-        if($paymentDetails['status'])
-        {
+        if ($paymentDetails['status']) {
             // There was no error and payment details has been retrieved
             // Now lets save the payment details and redirect the user to thanks you page
             $savedPaymentDetails = $this->paymentService->updatePaymentDetails($paymentDetails['paymentDetails'], $paymentDetails['paypalPaymentId']);
-            
-            if($savedPaymentDetails)
-            {
+
+            if ($savedPaymentDetails) {
                 // Return with booking and payment details and status as true
                 $bookingDetails = $this->bookingServices->getBookingSlotById($savedPaymentDetails->booking_id)->with('doctor')->first();
 
-                if($bookingDetails)
-                {
+                if ($bookingDetails) {
                     $bookingDate = getFormattedDate($bookingDetails->booking_date);
                     $bookingSlotTime = 'Slot Time: ' . $bookingDetails->slot_start_time . ' - ' . $bookingDetails->slot_end_time;
                     $doctorName = $bookingDetails->doctor->first_name . ' ' . $bookingDetails->doctor->last;
@@ -98,9 +96,7 @@ class PaymentApiController extends Controller
                 //     'message'   =>  "Payment details updated successfully!"
                 // ], 200);
             }
-        }
-        else
-        {
+        } else {
             // There was some error with provided token
             // Now lets redirect the user to error page
             return response()->json([
@@ -111,9 +107,9 @@ class PaymentApiController extends Controller
     }
 
     /**
-    *   Update status for cancelled payments
-    */
-    
+     *   Update status for cancelled payments
+     */
+
     public function updatePaymentStatus(Request $request)
     {
         $paypalPaymentId = $request->query('token') ?? '';
@@ -125,29 +121,52 @@ class PaymentApiController extends Controller
         // }
 
         // Update the payment status as cancelled
-        $updatedPaymentDetails = $this->paymentService->updatePaymentStatus('Cancelled',$paypalPaymentId);
+        $updatedPaymentDetails = $this->paymentService->updatePaymentStatus('Cancelled', $paypalPaymentId);
 
         // Now cancel the appointment because the payment has been cancelled
-        $updatedBookingDetails = $this->bookingServices->updateStatus('cancelled',$updatedPaymentDetails->booking_id);
+        $updatedBookingDetails = $this->bookingServices->updateStatus('cancelled', $updatedPaymentDetails->booking_id);
 
         $bookingDetails = $this->bookingServices->getBookingSlotById($updatedPaymentDetails->booking_id)->with('doctor')->first();
-        if($bookingDetails)
-            {
-                $bookingDate = getFormattedDate($bookingDetails->booking_date);
-                $bookingSlotTime = 'Slot Time: ' . $bookingDetails->slot_start_time . ' - ' . $bookingDetails->slot_end_time;
-                $doctorName = $bookingDetails->doctor->first_name . ' ' . $bookingDetails->doctor->last;
-                $errorPageHtml = view('doctor.error', compact('bookingDate', 'bookingSlotTime', 'doctorName'))->render();
-                return $errorPageHtml;
-            }
-
-        // Return with booking and payment details and status as true
-        // return response()->json([
-        //     'status'    =>  true,
-        //     'data'      =>  [
-        //         'payment'   =>  $updatedPaymentDetails,
-        //         'booking'   =>  $this->bookingServices->getBookingSlotById($updatedPaymentDetails->booking_id)->first()
-        //     ],
-        //     'message'   =>  "Payment status updated successfully!"
-        // ], 200);
+        if ($bookingDetails) {
+            $bookingDate = getFormattedDate($bookingDetails->booking_date);
+            $bookingSlotTime = 'Slot Time: ' . $bookingDetails->slot_start_time . ' - ' . $bookingDetails->slot_end_time;
+            $doctorName = $bookingDetails->doctor->first_name . ' ' . $bookingDetails->doctor->last;
+            $errorPageHtml = view('doctor.error', compact('bookingDate', 'bookingSlotTime', 'doctorName'))->render();
+            return $errorPageHtml;
+        }
+    }
+    public function getDoctorPaymentDetails(Request $request)
+    {
+        try {
+            $allPatientPaymentDetails = $this->paymentService->getPaymentDetailsByDoctorId(Auth::guard('api')->user()->id, $request->all());
+            return response()->json([
+                'status' => true,
+                'message' => "Retrieved All Patient Payment Details",
+                'data' => $allPatientPaymentDetails
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => false,
+                "error" =>  $e->getMessage(),
+                "message" => "Unable to find Payment Details"
+            ], 500);
+        }
+    }
+    public function getPatientPaymentDetails(Request $request)
+    {
+        try {
+            $allDoctorPaymentDetails = $this->paymentService->getPaymentDetailsByPatientId(Auth::guard('api')->user()->id, $request->all());
+            return response()->json([
+                'status' => true,
+                'message' => "Retrieved All Doctor Payment Details",
+                'data' => $allDoctorPaymentDetails
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => false,
+                "error" =>  $e->getMessage(),
+                "message" => "Unable to find Payment Details"
+            ], 500);
+        }
     }
 }
